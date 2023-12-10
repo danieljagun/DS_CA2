6af8d03e413c2940facaccb3978c1a13717ecdb2
Daniel J <daniel@daniels-air-3.home>
2023-12-10 20:04:30 +0000

    first commit

---
+++ src/main/java/com/example/dsca2/Dsca2Application.java
@@ -0,0 +1,13 @@
+package com.example.dsca2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsca2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dsca2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dsca2/Dsca2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsca2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsca2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
